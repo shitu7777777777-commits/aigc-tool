8afa26a037449ab66bb84b58a766c7465f203511
AIGC Developer <dev@aigc-tool.com>
2026-03-17 23:03:01 +0800

    Fix worker.ts import path

--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,4 @@
-import config from '../config/index';
+import config from './config/index';
 import { popTask, getQueueLength } from '../lib/redis';
 import { processTask } from '../services/task';
 
